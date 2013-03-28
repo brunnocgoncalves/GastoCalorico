a4d8d0f0da440bb7ff4a669657931c5fcbfb5d01
Brunno Gonçalves <brunnocgoncalves@gmail.com>
2013-03-27 23:37:46 -0300

    Fix

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -1,14 +1,10 @@
 // Wait for Cordova to load
 //
-document.addEventListener("deviceready", onDeviceReady, false);
-
-// Cordova is ready
-//
-function onDeviceReady() {
-	var parentElement = document.getElementById('deviceready');
+document.addEventListener("deviceready", function(){
+    var parentElement = document.getElementById('deviceready');
 	var listeningElement = parentElement.querySelector('.listening');
 	var receivedElement = parentElement.querySelector('.received');
 
 	listeningElement.setAttribute('style', 'display:none;');
 	receivedElement.setAttribute('style', 'display:block;');
-}
+ },true);
